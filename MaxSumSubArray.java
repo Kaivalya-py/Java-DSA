b4258977b53c00e97c24b61c135c85c9150a8307
KaivalyaVIT <kaivalya.sao23@vitstudent.ac.in>
2024-02-02 23:30:17 +0530

    Maximum sum of sub array problem done using Kadane's Algorithm in Java

---
+++ MaxSumSubArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class MaxSumSubArray {
+	static int maxSumSubArray(int[] arr, int n) {
+		int maxTillNow = arr[0];
+		int maxEnd = 0;
+		for (int i = 0; i < n; i++) {
+			maxEnd += arr[i];
+			if (maxEnd < 0)
+				maxEnd = 0;
+			else if (maxEnd > maxTillNow)
+				maxTillNow = maxEnd;
+		}
+		return maxTillNow;
+	}
+
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		int n = inp.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = inp.nextInt();
+		}
+		System.out.println(maxSumSubArray(arr, n));
+		inp.close();
+	}
+}
