652e3f98068bdca77281e6c430b060a384839d91
KaivalyaVIT <kaivalya.sao23@vitstudent.ac.in>
2024-02-03 00:28:33 +0530

    Added optimal solution for all test cases

--- MaxSumSubArray.java
+++ MaxSumSubArray.java
@@ -1,18 +1,36 @@
-import java.util.Scanner;
-
+import java.util.*;
 class MaxSumSubArray {
-	static int maxSumSubArray(int[] arr, int n) {
-		int maxTillNow = arr[0];
+	static int maxSumSubArray(int[] nums) {
+		int maxTillNow = nums[0];
 		int maxEnd = 0;
-		for (int i = 0; i < n; i++) {
-			maxEnd += arr[i];
-			if (maxEnd < 0)
+        int n = nums.length;
+        if(n==1)
+            return (nums[0]);
+		for (int i = 0; i < nums.length; i++) {
+			maxEnd += nums[i];
+            if (maxEnd < 0)
 				maxEnd = 0;
-			else if (maxTillNow < maxEnd)
+            else if (maxTillNow<maxEnd)
 				maxTillNow = maxEnd;
 		}
-		return maxTillNow;
+		Arrays.sort(nums);
+        if(nums[n-1]<0)
+            return(nums[n-1]);
+        return(maxTillNow);  
 	}
+
+	static int maxSubArrayOpt(int[] nums) {
+        int sum = 0;
+        int max_sum = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            max_sum = sum > max_sum ? sum : max_sum;
+            if(sum < 0)
+                sum = 0;
+        }
+
+        return max_sum;
+    }
 
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
@@ -21,7 +39,7 @@ class MaxSumSubArray {
 		for (int i = 0; i < n; i++) {
 			arr[i] = inp.nextInt();
 		}
-		System.out.println(maxSumSubArray(arr, n));
+		System.out.println(maxSumSubArray(arr));
 		inp.close();
 	}
 }
